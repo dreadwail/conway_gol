190a32b51b29e75358ae2ff06f8c308a8562d2b3
dreadwail <dreadwail42@gmail.com>
2015-02-25 09:46:19 -0800

    Test isolated cells die

--- test/brain_test.rb
+++ test/brain_test.rb
@@ -26,4 +26,16 @@ class BrainTest < Minitest::Test
     assert_empty subject.cells
   end
 
+  def test_isolated_cells_die
+    [
+      ConwayGameOfLife::Brain::Cell.new(0, 0),
+      ConwayGameOfLife::Brain::Cell.new(2, 2),
+      ConwayGameOfLife::Brain::Cell.new(4, 4)
+    ].each do |cell|
+      subject.cells << cell
+    end
+    subject.tick
+    assert_empty subject.cells
+  end
+
 end
